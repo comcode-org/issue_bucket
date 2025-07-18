54ecec3442b19c64b7724761a85be4ef13f45d5f
kbinreallife <141305609+kbinreallife@users.noreply.github.com>
2025-07-18 23:57:13 +0000

    let bread

---
+++ bread.js
@@ -0,0 +1,14 @@
+function bread() {
+
+    let bread = [
+        "",
+        "`I  ______     `",
+        "`I ( .  . ) W  `",
+        "`I  |  u |_/|  `",
+        "`I  |____|  |  `"
+    ];
+
+    return bread.join("\n");
+}
+
+console.log(bread)
